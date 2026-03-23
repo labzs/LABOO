651309f501653141594dfc4706afaaccaede14c6
Lara Santana <103907370+labzs@users.noreply.github.com>
2026-03-23 14:17:42 -0300

    Update Postagem.cpp

--- Exercicios/Postagem.cpp
+++ Exercicios/Postagem.cpp
@@ -1,1 +1,8 @@
 
+void Postagem::assistir() {
+    visualizacoes++; // Incrementa o valor atual em 1
+}
+
+void Postagem::imprimir() {
+    cout << "Postagem: " << legenda << " - " << visualizacoes << " visualizacoes" << endl;
+}
