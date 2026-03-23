9bfc6a95ceb8655290cdc122f2dc949b11d02ad4
Lara Santana <103907370+labzs@users.noreply.github.com>
2026-03-23 14:18:14 -0300

    Update Perfil.h

--- Exercicios/Perfil.h
+++ Exercicios/Perfil.h
@@ -1,1 +1,11 @@
 
+class Perfil {
+public:
+  string nome;
+  Postagem* p1 = NULL;
+  Postagem* p2 = NULL;
+  int quantidade = 0;
+
+  int getTotalDeVisualizacoes();
+  bool postar(Postagem* p);
+};
