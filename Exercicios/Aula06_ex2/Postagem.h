42db1450c94750347959ad306b5706d38c38f20b
Lara <larasantana@usp.br>
2026-04-27 15:18:18 -0300

    Cria .h e .cpp para subclasse Video ex2

--- Exercicios/Aula06_ex2/Postagem.h
+++ Exercicios/Aula06_ex2/Postagem.h
@@ -22,22 +22,4 @@ public:
  ~Postagem();
   
 };
-
-
-//DECLARAÇÃO DA SUBCLASSE:
-//class NomeSubClasse: public NomeSuperClasse {}
-
-class Video: public Postagem {
-protected:
-int duracao = 0;
-
-//MODO DE VISIBILIDADE
-public: 
- Video(string legenda, int duracao);
- ~Video();
- int getDuracao();
- void assistir(int tempo);
-};
-
-
 #endif
