4695dfa84cc9ffde4ff1dba80f508bc4770cd346
Lara Santana <larasantana@usp.br>
2026-05-17 15:00:38 -0300

    Implementa função teste

--- Exercicios/Aula07_ex2/Postagem.h
+++ Exercicios/Aula07_ex2/Postagem.h
@@ -9,13 +9,14 @@ using namespace std;
 class Postagem {
 protected: 
   string legenda;  //ATRIBUTO
+  Postagem** postagens;
   int visualizacoes = 0;
 public:
   string getLegenda();
   void setLegenda(string leg);
   int getVisualizacoes();
   void assistir ();    //MÉTODOS
-  void imprimir ();
+  virtual void imprimir ();
  
 // Construtor
  Postagem(string legenda);
--- Exercicios/Aula07_ex2/teste.cpp
+++ Exercicios/Aula07_ex2/teste.cpp
@@ -10,46 +10,49 @@ using namespace std;
 
 void teste() {
 
-   //
-   // a. Crie 3 Conteudos, com legendas “c1” (assista 1 vez), “c2” (assista 2 vezes) e “c3” (assista 3 vezes) criados nessa mesma ordem;
-    Conteudo* c1 = new Conteudo("c1");
-    Conteudo* c2 = new Conteudo("c2");
-    Conteudo* c3 = new Conteudo("c3");
+//a. Crie 5 vídeos, com legendas “v1”, “v2”, “v3”, “v4”, “v5”, criados nessa mesma ordem e todos com duração 5.
 
-    for (int i = 0; i < 1; i++) {
-        c1->assistir();
-    }
-     for (int i = 0; i < 2; i++) {
-        c2->assistir();
-    }
-    for (int i = 0; i < 3; i++) {
-        c3->assistir();
-    }
+Video* v1 = new Video("v1",5);
+Video* v2 = new Video("v2",5);
+Video* v3 = new Video("v3",5);
+Video* v4 = new Video("v4",5);
+Video* v5 = new Video("v5",5);
 
-   // b. Crie 1 Video, com legenda “v1” e duracao 5 (assista 4 vezes, com o argumento
 
-   Video* v1 = new Video("v1", 5);
-    for (int i = 0; i < 4; i++) {
-          v1->assistir(4); 
-     }
+//b. Crie 2 Listas, de legendas “l1”, “l2” e ambas com tamanho 5.
+Lista* l1 = new Lista("l1",5);
+Lista* l2 = new Lista("l2",5);
 
-    //c. Crie 1 Lista, de legenda “l1” e com tamanho 5.
-        Lista* l1 = new Lista("l1", 5);
-    
-    //d. Adicione os Conteudos (incluindo o Video) à Lista, na ordem em que foram criados;
-        l1->adicionar(c1);
-        l1->adicionar(c2);
-        l1->adicionar(c3);
-        l1->adicionar(v1);
 
-// e. Imprima a Lista;
-     l1->imprimir();
-//f. Delete os Conteudos, o Video e, por fim, a Lista.
-     delete c1;
-     delete c2;
-     delete c3;
-     delete v1;
-     delete l1;
+//c. Adicione os vídeos “v1”, “v3” e “v5” na lista com legenda “l1”. Adicione os outros vídeos criados anteriormente na lista “l2”.
+l1->adicionar(v1);
+l1->adicionar(v3);
+l1->adicionar(v5);
+l1->imprimir();
+
+l2->adicionar(v2);
+l2->adicionar(v4);
+
+//d. Adicione a Lista “l2” em “l1”.
+l1->adicionar(l2);
+
+l1->imprimir();
+//e. Crie um Perfil de nome “Jorge” e maximo 2.
+Perfil* p1 = new Perfil("Jorge",2);
+
+//f. Faça “Jorge” postar “l1”;
+p1->postar(l1);
+
+
+
+//g. Crie um Perfil a partir de seu construtor sobrecarregado, de nome “Roberto”, com postagens igual ao perfil de nome “Jorge” e de quantidade 1
+
+Perfil* p2 = new Perfil("Roberto",p1->getPostagens(),1);
+
+
+//h. Imprima o Perfil de nome “Roberto”. 
+
+//p2->imprimir();
 
 };
 
